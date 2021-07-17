cb93dabb597767272ceec5bc91e4cf9c6e043052
sydneyw-cyber <sydneyw521@gmail.com>
2021-07-17 11:42:57 -0500

    login

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -29,6 +29,48 @@ router.post('/', async (req, res) => {
     }
   });
   
+  // Login
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = await User.findOne( { where: {
+      password: req.body.password,
+    },
+  }
+);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 
   module.exports = router
   
