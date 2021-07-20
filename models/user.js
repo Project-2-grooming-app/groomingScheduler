b9ab71d2d23e07c919c89be61d948d9828e9a574
kashelton17 <kashelton17@gmail.com>
2021-07-20 19:50:41 -0400

    fixing issue with heroku app and user model

--- models/user.js
+++ models/user.js
@@ -14,13 +14,10 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    /* 
     username: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    */
-   
     email: {
       type: DataTypes.STRING,
       allowNull: false,
