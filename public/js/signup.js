d95e87f19d9e12202155b922d342df45776e300e
kashelton17 <kashelton17@gmail.com>
2021-07-20 19:40:05 -0400

    adding reservation time slots to homepage

--- public/js/signup.js
+++ public/js/signup.js
@@ -15,7 +15,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/groomer/');
       } else {
         alert('Failed signup');
       }
