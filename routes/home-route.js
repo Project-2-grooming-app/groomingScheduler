3b1f40a8c92e29e02ea003ee230860668accf32e
sydneyw-cyber <sydneyw521@gmail.com>
2021-07-20 20:51:37 -0500

    some css and bootstrap. also testing heroku

--- routes/home-route.js
+++ routes/home-route.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
             include: [
                 {
                     model: User,
-                    attributes: ['username']
+                    attributes: ['id']
                 }
             ]
         })
