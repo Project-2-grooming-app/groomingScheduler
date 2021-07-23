7f97a22a7b636680c922559f3041693a88f01850
kashelton17 <kashelton17@gmail.com>
2021-07-22 21:31:45 -0400

    fixing the redirect when a groomer is not logged in to login page

--- routes/groomer-routes.js
+++ routes/groomer-routes.js
@@ -2,32 +2,36 @@ const router = require('express').Router();
 const { User, Reservation, Schedule } = require('../models');
 
 router.get('/', async (req, res) => {
-    try {
-        const reserveData = await Reservation.findAll({
-            include: [
-                {
-                    model: Schedule,
-                    attributes: ['id', 'date_time']
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+      } else {
+        try {
+            const reserveData = await Reservation.findAll({
+                include: [
+                    {
+                        model: Schedule,
+                        attributes: ['id', 'date_time']
+                    }
+                ]
+            })
+            const scheduleData = await Schedule.findAll({
+                where: {
+                    available: true
                 }
-            ]
-        })
-        const scheduleData = await Schedule.findAll({
-            where: {
-                available: true
-            }
-        })
-        
-        const reservations = reserveData.map((reserve => 
-            reserve.get({ plain: true}))
-        )
-        const schedules = scheduleData.map((sched => 
-            sched.get({ plain: true})))
-        console.log(reservations)
-        res.render('groomers', { reservations, schedules, loggedIn: req.session.loggedIn})
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
+            })
+            
+            const reservations = reserveData.map((reserve => 
+                reserve.get({ plain: true}))
+            )
+            const schedules = scheduleData.map((sched => 
+                sched.get({ plain: true})))
+            console.log(reservations)
+            res.render('groomers', { reservations, schedules, loggedIn: req.session.loggedIn})
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
+      }
 })
 
 module.exports = router
